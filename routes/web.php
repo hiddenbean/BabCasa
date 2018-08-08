12553d735795cd82eb37cf0b55c2cac9aa62de8c
hoss dev <devhoss529@gmail.com>
2018-08-08 11:43:11 +0100

    init master

---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// GET DOMAINs
+Route::domain('www.babcasa.com')->group(function (){
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+   
+
+});
+
+Route::domain('staff.babcasa.com')->group(function (){
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+   
+
+});
+
+Route::domain('partner.babcasa.com')->group(function (){
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+   
+
+});
+
+
+
+
+// POST DOMAINs
+Route::domain('www.babcasa.com')->group(function (){
+
+    Route::post('/','');
+   
+
+});
+
+Route::domain('partner.babcasa.com')->group(function (){
+
+    Route::post('/','');
+   
+
+});
+
+Route::domain('staff.babcasa.com')->group(function (){
+
+    Route::post('/','');
+   
+
+});
