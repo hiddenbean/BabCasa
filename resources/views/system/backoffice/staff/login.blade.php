c668d7bc6cb041b5a54e97d4ad85bf4c5d9c9b03
Hicham Msaaf <hichammsaafdev@gmail.com>
2018-10-02 11:14:14 +0100

    Fr dictionary added

--- resources/views/system/backoffice/staff/login.blade.php
+++ resources/views/system/backoffice/staff/login.blade.php
@@ -1,83 +1,52 @@
-@extends('layouts.backoffice.app') 
+@extends('system.backoffice.staff.public_template') 
 
-@section('css') 
-
-@stop 
-
-@section('body')
-<div class="register-container sm-p-t-30 register-staff-width" >
-    <div class="d-flex justify-content-center flex-column full-height ">
-        <div class="logo_text">  
-            <div class="logo_text">
-                <img src="{{ asset('img/logo.png') }}" alt="{{ config('app.name', 'BAB Casa') }}" height="60"/>
-            </div>
-        </div>
-        <h3>Login with your BABCasa staff account</h3>
-        <div class="row">
-            <div class="col-md-12">
-                <form class="p-t-15" role="form" method="POST" action="{{route('staff.login.submit')}}">
-                    @csrf
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group form-group-default input-group @if($errors->has('username')) has-error @endif">
-                                <div class="form-input-group">
-                                    <label>USERNAME</label>
-                                    <input type="text" name="username" class="form-control" placeholder="Your staff username here">
-                                    @if ($errors->has('username'))
-                                    <label class='error' for='username'>{{ $errors->first('username') }}</label>
-                                    @endif
-                                </div>
-                                <div class="input-group-append ">
-                                    <span class="input-group-text">
-                                        @babcasa.com
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group form-group-default @if($errors->has('password')) has-error @endif">
-                                <label>PASSWORD</label>
-                                <input type="password" name="password" class="form-control" placeholder="Your staff password here">
-                                @if ($errors->has('password'))
-                                <label class='error' for='password'>{{ $errors->first('password') }}</label>
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <form class="p-t-15" role="form" method="POST" action="{{route('staff.login.submit')}}">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group form-group-default input-group @if($errors->has('username')) has-error @endif">
+                            <div class="form-input-group">
+                                <label>USERNAME</label>
+                                <input type="text" name="username" class="form-control" placeholder="Your staff username here"/>
+                                @if ($errors->has('username'))
+                                <label class='error' for='username'>{{ $errors->first('username') }}</label>
                                 @endif
                             </div>
-                        </div>
-                    </div>
-                    <div class="row m-t-10 m-b-10"> 
-                        <div class="col-md-6 sm-p-l-10">
-                            <div class="checkbox no-margin">
-                                <input type="checkbox" name="remember" id="remember">
-                                <label for="remember">Keep Me Signed in</label>
+                            <div class="input-group-append ">
+                                <span class="input-group-text">
+                                    @babcasa.com
+                                </span>
                             </div>
-                        </div> 
-                        <div class="col-md-6 text-right">
-                            <a href="{{ route('staffs.passwords.rest') }}" class="small">Forgot your password ?</a> 
                         </div>
                     </div>
-                    <button class="btn btn-primary btn-cons m-t-10" type="submit">Login</button>
-                </form>
-            </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group form-group-default @if($errors->has('password')) has-error @endif">
+                            <label>PASSWORD</label>
+                            <input type="password" name="password" class="form-control" placeholder="Your staff password here"/>
+                            @if ($errors->has('password'))
+                            <label class='error' for='password'>{{ $errors->first('password') }}</label>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-t-10 m-b-10"> 
+                    <div class="col-md-6 sm-p-l-10">
+                        <div class="checkbox no-margin">
+                            <input type="checkbox" name="remember" id="remember"/>
+                            <label for="remember">Keep Me Signed in</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <a href="{{ route('staffs.passwords.rest') }}" class="small">Forgot your password ?</a> 
+                    </div>
+                </div>
+                <button class="btn btn-primary btn-cons m-t-10" type="submit">Login</button>
+            </form>
         </div>
     </div>
-</div>
-
-<div class=" container-fluid  container-fixed-lg footer">
-    <div class="copyright sm-text-center">
-        <p class="small no-margin pull-left sm-pull-reset">
-            <span class="hint-text">Copyright © 2018 </span>
-            <span class="font-montserrat">BABCasa</span>.
-            <span class="hint-text">All rights reserved. </span>
-            <span class="sm-block"><a href="#" class="m-l-10 m-r-10">Terms of use</a> <span class="muted">|</span> <a href="#" class="m-l-10">Privacy Policy</a></span>
-        </p>
-        <p class="small no-margin pull-right sm-pull-reset">
-            <a href="javascript:;">English (United States)</a>
-        <div class="clearfix"></div>
-    </div>
-</div>
 @stop 
-
-@section('script')
-    @stop
--- resources/views/system/backoffice/staff/password/email.blade.php
+++ resources/views/system/backoffice/staff/password/email.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.backoffice.app') 
-@section('css') @stop 
 
 @section('body')
 
@@ -49,10 +48,38 @@
                 <span class="sm-block"><a href="#" class="m-l-10 m-r-10">Terms of use</a> <span class="muted">|</span> <a href="#" class="m-l-10">Privacy Policy</a></span>
             </p>
             <p class="small no-margin pull-right sm-pull-reset">
-                <a href="javascript:;">English (United States)</a>
+                <a id="langs" href="javascript:;">{{ LaravelLocalization::getCurrentLocaleName() }} ({{ LaravelLocalization::getCurrentLocaleRegional() }})</a>
+                <div class="dropup-content">
+                    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        <a class="small" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                            {{ $properties['native'] }} ({{ $properties['regional'] }})
+                        </a>
+                    @endforeach
+                </div>
+            </p>
             <div class="clearfix"></div>
         </div>
     </div>
 @stop 
 
-@section('script') @stop
+@section('script')
+    <script>
+    $('#langs').click(function() {
+        $('.dropup-content').show();
+    });
+
+    $('.dropup-content a').click(function() {
+        $('.dropup-content').hide();
+    });
+
+    $(document).mouseup(function(e) 
+    {
+        var container = $(".dropup-content");
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+            container.hide();
+        }
+    });
+    </script>
+@stop
