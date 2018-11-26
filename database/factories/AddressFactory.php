58e7b45bac2c864b2fcc01137072c47bdeb69ce3
rof_hicham <hichammsaafdev@gmail.com>
2018-11-26 15:38:38 +0100

    Seeds Fixs

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -10,6 +10,7 @@ $factory->define(App\Address::class, function (Faker $faker) {
         'address_two' => str_random(10),
         'full_name' => str_random(10),
         'zip_code' => '12345678',
+        'is_default' => 1,
         'latitude' => '34.345678',
         'longitude' => '34.56789',
         'addressable_id' => '1',
